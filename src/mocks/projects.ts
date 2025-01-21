c19475f954b7a4132ae63cca45ac20364daffde5
gabrielfula <gabriel.fulaneto0301@gmail.com>
2025-01-21 00:22:00 -0300

    chore: change text Ecommerce

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,6 +1,6 @@
 export const projectsData = [
      {
-         title: "Eccomerce",
+         title: "Ecommerce",
          description: "Apresento a vocês o projeto de Ecommerce desenvolvido com React JS, Styled-Components, Axios, React-Router-dom e a API do Mercado Livre combina tecnologias modernas e poderosas para criar uma plataforma de comércio eletrônico dinâmica e visualmente atraente. Com essa solução, os usuários poderão desfrutar de uma experiência única. A integração com a API do Mercado Livre nos permite exibir um catálogo atualizado de produtos, pois podemos buscar informações em tempo real diretamente da plataforma. Isso garante que os usuários tenham acesso a uma ampla variedade de produtos disponíveis para compra, tornando o ecommerce mais atrativo e funciona.",
          stack: "React JS, Styled-Components, React-Router-dom, Axios",
          deploy: "https://gabrielfula-ecommerce.netlify.app",
